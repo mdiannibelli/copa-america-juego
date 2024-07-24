02c3b75cd035e783cc298ff9e91e93afde5d5595
mdiannibelli <mdiannibelli@gmail.com>
2024-07-24 19:09:36 -0300

    fixing url api

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = import.meta.env.PROD ? 'https://copa-america-juego.vercel.app' : 'http://localhost:5173/data.json';
+export const API_URL = import.meta.env.PROD ? 'https://copa-america-juego.vercel.app/data.json' : 'http://localhost:5173/data.json';
 export const MAX_QUESTIONS = 25;
