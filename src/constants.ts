6b912df4060bf92d103f9f5562ca37cf5de90807
mdiannibelli <mdiannibelli@gmail.com>
2024-07-24 19:06:15 -0300

    fix fetch url

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:5173/data.json';
+export const API_URL = import.meta.env.PROD ? 'https://copa-america-juego.vercel.app' : 'http://localhost:5173/data.json';
 export const MAX_QUESTIONS = 25;
